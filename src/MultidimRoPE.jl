47952c19d4421e37369de5d99986663db9aa3510
Jack Elliot Collier Ryder <jack.collier.ryder@gmail.com>
2025-08-08 16:12:32 +0200

    Fix dispatch issue

--- src/MultidimRoPE.jl
+++ src/MultidimRoPE.jl
@@ -56,19 +56,19 @@ function (rope::MultidimRoPE)(x::AbstractArray, positions::AbstractArray)
     return reshape(rotated_x, size(x))
 end
 
-function (block::TransformerBlock)(x, positions; rope=identity, mask=0)
+function (block::TransformerBlock)(x::AbstractArray, positions::AbstractArray; rope=identity, mask=0)
     h = x + block.attention(block.attention_norm(x), positions, rope; mask)
     out = h + block.feed_forward(block.ffn_norm(h))
     return out
 end
-(block::TransformerBlock)(x, positions, rope=identity, mask=0) = block(x, positions; rope, mask)
+(block::TransformerBlock)(x::AbstractArray, positions::AbstractArray, rope=identity, mask=0) = block(x, positions; rope, mask)
 
-function (block::AdaTransformerBlock)(x, positions, cond; rope=identity, mask=0)
+function (block::AdaTransformerBlock)(x::AbstractArray, positions::AbstractArray, cond; rope=identity, mask=0)
     h = x + block.attention(block.attention_norm(x, cond), positions, rope; mask)
     out = h + block.feed_forward(block.ffn_norm(h, cond))
     return out
 end
-(block::AdaTransformerBlock)(x, positions, cond, rope=identity, mask=0) = block(x, positions, cond; rope, mask)
+(block::AdaTransformerBlock)(x::AbstractArray, positions::AbstractArray, cond, rope=identity, mask=0) = block(x, positions, cond; rope, mask)
 
 function (attn::Attention)(x::AbstractArray, positions::AbstractArray, rope::MultidimRoPE; mask=0)
     return attn(x, x, positions, rope; mask)
