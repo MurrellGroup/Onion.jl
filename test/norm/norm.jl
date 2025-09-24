d158ed23821a2c51a9876768036b4702268a2dfc
Anton Oresten <antonoresten@proton.me>
2025-09-24 13:12:45 +0200

    add p=5 test case

--- test/norm/norm.jl
+++ test/norm/norm.jl
@@ -1,7 +1,7 @@
 @testset "norm.jl" begin
     @testset "LpNorm" begin
         v = randn(Float32, 10, 20)
-        for p in 1:4
+        for p in 1:5
             @test sum(x -> abs(x)^p, LpNorm(p, dims=(1,2))(v)) ≈ 1f0
             @test all(x -> abs(x) ≈ 1f0, sum(x -> abs(x)^p, LpNorm(p, dims=1)(v), dims=1))
             @test all(x -> abs(x) ≈ 1f0, LpNorm(p, dims=())(v))
