32163af9e8deeddf35dfda8195f59f098e2c3b5b
Anton Oresten <antonoresten@proton.me>
2025-09-24 12:34:32 +0200

    fix norm tests

--- test/norm/norm.jl
+++ test/norm/norm.jl
@@ -1,9 +1,9 @@
 @testset "norm.jl" begin
     @testset "PNorm" begin
         v = randn(Float32, 10)
-        @test sum(abs, PNorm(1)(v)) ≈ 1f0
-        @test PNorm(1, dims=())(v) == v
-        @test sum(abs2, PNorm(2)(v)) ≈ 1f0
-        @test PNorm(2, dims=())(v) == v
+        for p in 1:4
+            @test sum(x -> abs(x)^p, PNorm(p)(v)) ≈ 1f0
+            @test all(x -> abs(x) ≈ 1f0, PNorm(p, dims=())(v))
+        end
     end
 end
