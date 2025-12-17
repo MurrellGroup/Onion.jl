023c3e669d44e814cfadaf21770186f1516a6360
AntonOresten <antonoresten@proton.me>
2025-12-17 16:00:12 +0100

    Remove `layer` field from VirtualWidthNetwork

--- src/miscellaneous/VirtualWidthNetwork.jl
+++ src/miscellaneous/VirtualWidthNetwork.jl
@@ -3,7 +3,8 @@ using ChainRulesCore
 using NNlib
 
 """
-    VirtualWidthNetwork(layer, n, m)
+    VirtualWidthNetwork(n, m)
+    (vwn::VirtualWidthNetwork)(layer, h::AbstractArray)
 
 Wrap a sublayer (e.g. attention or FFN) with the static form of
 **Generalized Hyper-Connections (GHC)**.
@@ -14,7 +15,7 @@ segments. This layer:
 
 - **compresses** an over-width state of size \$\\frac{n}{m}D\$ down
 to backbone width \$D\$ by projecting down the n segments into m segments,
-- applies the wrapped layer at backbone width,
+- applies `layer` at backbone width,
 - **expands** the backbone output back to n segments,
 - **carries** forward the previous over-width state with a projection
 from n segments to n segments, adding it to the expanded backbone output.
@@ -23,27 +24,25 @@ See: [Virtual Width Networks](https://arxiv.org/abs/2511.11238)
 """
 @concrete struct VirtualWidthNetwork <: Layer
     down; side; up
-    layer
 end
 
-function VirtualWidthNetwork(layer, n, m)
-    r = n - m
-    down = Float32[I(m); zeros(r, m)]
+function VirtualWidthNetwork(n, m)
+    down = Float32[I(m); zeros(n - m, m)]
     side = Float32[I(n);]
-    up   = Float32[repeat(I(m), 1, fld(n, m));; I(r); zeros(r, m - r)]
-    return VirtualWidthNetwork(down, side, up, layer)
+    up   = Float32[repeat(I(m), 1, fld(n, m));; I(mod(n, m)); zeros(m - mod(n, m), mod(n, m))]
+    return VirtualWidthNetwork(down, side, up)
 end
 
-function (vwn::VirtualWidthNetwork)(h, ::typeof(einsum))
+function (vwn::VirtualWidthNetwork)(layer, h::AbstractArray, ::typeof(einsum))
     x  = einsum(h, vwn.down, einops"(d n) ..., n m -> (d m) ...")
-    z  = vwn.layer(x)
+    z  = layer(x)
     h′ = einsum(z, vwn.up, einops"(d m) ..., m n -> (d n) ...") +
         einsum(h, vwn.side, einops"(d n₁) ..., n₁ n₂ -> (d n₂) ...")
     return h′
 end
 
-function (vwn::VirtualWidthNetwork)(h)
-    (; down, side, up, layer) = vwn
+function (vwn::VirtualWidthNetwork)(layer, h::AbstractArray)
+    (; down, side, up) = vwn
     n, m = size(down)
     H  = rearrange(h, einops"(d n) ... -> d n ..."; n)
     X  = batched_mul(H, down)
@@ -59,9 +58,10 @@ end
 function ChainRulesCore.rrule(
     config::RuleConfig{>:HasReverseMode},
     vwn::VirtualWidthNetwork,
-    h
+    layer,
+    h::AbstractArray
 )
-    (; down, side, up, layer) = vwn
+    (; down, side, up) = vwn
     n, m = size(down)
     H  = rearrange(h, einops"(d n) ... -> d n ..."; n)
     X  = batched_mul(H, down)
@@ -91,8 +91,8 @@ function ChainRulesCore.rrule(
 
         Δh = rearrange(ΔH, einops"d n ... -> (d n) ...")
 
-        Δvwn = Tangent{VirtualWidthNetwork}(; down=Δdown, side=Δside, up=Δup, layer=Δlayer)
-        return (Δvwn, Δh)
+        Δvwn = Tangent{VirtualWidthNetwork}(; down=Δdown, side=Δside, up=Δup)
+        return (Δvwn, Δlayer, Δh)
     end
 
     return h′, vwn_pullback
